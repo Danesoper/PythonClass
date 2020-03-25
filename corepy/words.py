99edbc563e99b5d90af2e352f13b060683cb4171
Dan Soper <dan.e.soper@gmail.com>
2020-03-25 08:20:16 -0500

    words.py is now a module

--- corepy/words.py
+++ corepy/words.py
@@ -1,13 +1,27 @@
 from urllib.request import urlopen
 
 
-story = urlopen('http://sixty-north.com/c/t.txt')
-story_words = []
-for line in story:
-    line_words = line.decode('utf-8').split()
-    for word in line_words:
-        story_words.append(word)
-story.close()
+def fetch_words(url):
 
-for word in story_words:
-    print(word)
+    story = urlopen(url)
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf-8').split()
+        for word in line_words:
+            story_words.append(word)
+    story.close()
+    return story_words
+
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+
+def main():
+    words = fetch_words()
+    print_items(words)
+
+
+if (__name__) == ('__main__'):
+    main()
